4e194ea8ecb09ba2d67059b0d72c68c1e34246af
Philip Knudsen <philipalexkndusen@gmail.com>
2022-09-26 09:39:27 +0200

    Add project files.

---
+++ Frontend/MainWindow.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace CustomerDatabaseLookup
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(343, 170);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(28, 17);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "tbd";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.nameLabel);
+            this.Name = "MainWindow";
+            this.Text = "Customer Database";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nameLabel;
+    }
+}
+
---
+++ Src/Models/Customer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerDatabaseLookup.Src.Models
+{
+    class Customer
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string FullName { get; private set; }
+        public string Description { get; private set; }
+        public int PhoneNumber { get; private set; }
+
+        public Customer( string firstName, string lastName, string description, int phoneNumber )
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            FullName = $"{firstName} {lastName}";
+            Description = description;
+            PhoneNumber = phoneNumber;
+        }
+
+        public Customer( string fullName, string description, int phoneNumebr )
+        {
+            FullName = fullName;
+            string[] h = fullName.Split(' ');
+            FirstName = h[0];
+            LastName = h[1];
+            Description = description;
+            PhoneNumber = phoneNumebr;
+        }
+
+        public void ChangeFullName( string fullName )
+        {
+            string[] h = fullName.Split(' ');
+            FirstName = h[0];
+            LastName = h[1];
+            FullName = fullName;
+        }
+
+    }
+}
