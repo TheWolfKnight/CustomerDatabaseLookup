fc5cadb859ee0a47c1de04a27873bf278d90bf96
Dannie Schytte <dannieschytte@gmail.com>
2022-10-10 10:36:31 +0200

    same

---
+++ Frontend/LogInWindow.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace CustomerDatabaseLookup.Frontend
+{
+    partial class LogInWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lb_2 = new System.Windows.Forms.Label();
+            this.tb_UserName = new System.Windows.Forms.TextBox();
+            this.tb_Password = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lb1
+            // 
+            this.lb1.AutoSize = true;
+            this.lb1.Location = new System.Drawing.Point(185, 119);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(104, 16);
+            this.lb1.TabIndex = 0;
+            this.lb1.Text = "Inter User Name";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(282, 263);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Log In";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lb_2
+            // 
+            this.lb_2.AutoSize = true;
+            this.lb_2.Location = new System.Drawing.Point(185, 186);
+            this.lb_2.Name = "lb_2";
+            this.lb_2.Size = new System.Drawing.Size(95, 16);
+            this.lb_2.TabIndex = 2;
+            this.lb_2.Text = "Inter Password";
+            // 
+            // tb_UserName
+            // 
+            this.tb_UserName.Location = new System.Drawing.Point(349, 119);
+            this.tb_UserName.Name = "tb_UserName";
+            this.tb_UserName.Size = new System.Drawing.Size(100, 22);
+            this.tb_UserName.TabIndex = 3;
+            // 
+            // tb_Password
+            // 
+            this.tb_Password.Location = new System.Drawing.Point(349, 186);
+            this.tb_Password.Name = "tb_Password";
+            this.tb_Password.Size = new System.Drawing.Size(100, 22);
+            this.tb_Password.TabIndex = 4;
+            // 
+            // LogInWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tb_Password);
+            this.Controls.Add(this.tb_UserName);
+            this.Controls.Add(this.lb_2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lb1);
+            this.Name = "LogInWindow";
+            this.Text = "LogInWindow";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.Label lb_2;
+        public System.Windows.Forms.TextBox tb_UserName;
+        public System.Windows.Forms.TextBox tb_Password;
+    }
+}
